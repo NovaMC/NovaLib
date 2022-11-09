48c0bac74960f1ebd3077696f4a7fb5ba6ee024a
Lui798 <42649201+Lui798@users.noreply.github.com>
2022-11-09 06:09:36 -0600

    Create a shell of a velocity plugin

---
+++ velocity/src/main/java/xyz/novaserver/core/Core.java
@@ -0,0 +1,28 @@
+package xyz.novaserver.core;
+
+import com.google.inject.Inject;
+import com.velocitypowered.api.event.Subscribe;
+import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
+import com.velocitypowered.api.plugin.annotation.DataDirectory;
+import com.velocitypowered.api.proxy.ProxyServer;
+import org.slf4j.Logger;
+
+import java.nio.file.Path;
+
+public class Core {
+    private final ProxyServer proxy;
+    private final Logger logger;
+    private final Path dataDirectory;
+
+    @Inject
+    public Core(ProxyServer server, Logger logger, @DataDirectory Path dataDirectory) {
+        this.proxy = server;
+        this.logger = logger;
+        this.dataDirectory = dataDirectory;
+    }
+
+    @Subscribe
+    public void onProxyInitialization(ProxyInitializeEvent event) {
+        //TODO: not implemented
+    }
+}
